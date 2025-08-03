d96a71cf3ca7055d176a90144d8f8ee12e4a5505
Kene33 <hamid.hokage2009@gmail.com>
2025-08-03 06:03:57 +0300

    Add return type annotation for delete_link method in RedisClient

--- src/app/db/client.py
+++ src/app/db/client.py
@@ -5,18 +5,15 @@ class RedisClient:
         self.pool = redis.ConnectionPool(host=host, port=port, db=db)
         self.client = redis.Redis(connection_pool=self.pool, decode_responses=True)
 
-    async def add_shortlink(self, key: str, value: str):
-        """
-        key = shotcode
-        """
-        await self.client.set(key, value)
-
+    async def add_shortlink(self, key: str, value: str) -> str:
+        added_link = await self.client.set(key, value)
+        return added_link
+    
     async def get_value_by_key(self, key: str) -> str | None:
         value = await self.client.get(key)
         value = value.decode('utf-8') if value else None
-
         return value
     
-    async def delete_link(self, key: str):
+    async def delete_link(self, key: str) -> int:
         deleted_link = await self.client.delete(key)
         return deleted_link
